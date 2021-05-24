973450833b1244b8c3716a34ef67c1071ee4f97b
sunil sharma <sunil68895@gmail.com>
2021-05-24 11:35:11 +0530

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const env=require('dotenv')
 const mongoose = require("mongoose");
+const cors = require("cors");
 env.config();
 require("./models/user");
 require("./models/post");
+
 
 mongoose.connect(process.env.MONGOURL, {
   useNewUrlParser: true,
@@ -17,7 +19,11 @@ mongoose.connection.on("connected", () => {
 mongoose.connection.on("error", (err) => {
   console.log("err connecting", err);
 });
-
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/postRoutes");
 const userRoutes = require("./routes/userRoutes");
